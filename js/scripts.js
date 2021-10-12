b67f4a686ee0aa4ce6c2a1ab9e10fdb17f8ee5ae
Neha Yadav <40368757+Nehayadav3055@users.noreply.github.com>
2021-10-12 10:49:40 -0400

    text updated. resume updated
    text updated. resume updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,2 @@
-/*!
-* Start Bootstrap - Neha Yadav Quality Assurance Engineer Website v1.0.0 (https://startbootstrap.com/theme/one-page-wonder)
-* Copyright 2013-2021 [object Object]
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
-*/
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
